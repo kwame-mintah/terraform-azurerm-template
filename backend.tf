97a47691b00befae8f605f2ac6b8a04642f06a89
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-08-02 17:19:13 +0100

    refactor(backend): provide `azurerm` config for the backend
    comments added to `backend.tf` providing instructions to follow. alternatively the storeaccount can be created manually as suggested [here](https://tsuyoshiushio.medium.com/terragrunt-quick-start-with-azure-4a9a09ab2e21). or follow the offical microsoft [guide](https://learn.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage?tabs=azure-cli) for store state in azure storage.

--- backend.tf
+++ backend.tf
@@ -1,5 +1,16 @@
+# (1) This block of code needs to be uncommented out first, before attempting to use the azurerm backend
+# After all configurations have been populated run `terragrunt init -migrate-state` in the directory
+# When prompted, answer yes and will copy the state file to the storage account.
+# Alternatively follow this guide:
+# https://learn.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage?tabs=azure-cli
+
 terraform {
   backend "local" {
     path = "terraformstate/terraform.tfstate"
   }
 }
+
+# (2) Only uncomment out this code block, after the tfstate storage account has been created
+# terraform {
+#   backend "azurerm" {}
+# }
